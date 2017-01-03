a7fa39e6d214715d39fa9409f9de5cb056ea10a3
ahmadjaradat <ahmad.jaradat@progressoft.com>
2017-01-03 13:27:13 -0800

    112

--- payment-parent/payment-web/src/main/java/com/progressoft/jip/ui/web/DemoServlet.java
+++ payment-parent/payment-web/src/main/java/com/progressoft/jip/ui/web/DemoServlet.java
@@ -8,13 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/asd")
-public class MainServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/demo")
+public class DemoServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestURI();
-		super.doGet(req, resp);
+		req.getRequestDispatcher("/WEB-INF/jsp/sample.jsp").forward(req, resp);
 	}
-
 }
