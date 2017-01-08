55e4e8c18f0688fbe0a3ebbcfbef11b56cd166e3
Anas Hamed <u620@Progressoft.com>
2017-01-08 15:02:31 +0200

    no message

--- payment-parent/payment-core/src/main/java/com/progressoft/jip/payment/report/impl/AbstractReportGenerator.java
+++ payment-parent/payment-core/src/main/java/com/progressoft/jip/payment/report/impl/AbstractReportGenerator.java
@@ -12,13 +12,13 @@ import com.progressoft.jip.payment.report.core.ReportNode;
 import com.progressoft.jip.payment.report.core.ReportSettingsSpi;
 
 public abstract class AbstractReportGenerator implements ReportGenerator {
-	private static final String AMOUNT_TRANSCRIPTION_TAG = "payment-amount-transcription";
-	private static final String BALANCE_TAG = "balance";
 	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractReportGenerator.class);
+	private static final String BALANCE_TAG = "balance";
 	private static final String PAYMENT_PURPOSE_TAG = "payment-purpose";
 	private static final String PAYMENT_DATE_TAG = "payment-date";
 	private static final String TRANSFER_CURRENCY_TAG = "transfer-currency";
 	private static final String PAYMENT_AMOUNT_TAG = "payment-amount";
+	private static final String PAYMENT_AMOUNT_TRANSCRIPTION_TAG = "payment-amount-transcription";
 	private static final String BENEFICIARY_NAME_TAG = "beneficiary-name";
 	private static final String BENEFICIARY_IBAN_TAG = "beneficiary-iban";
 	private static final String ORDERING_ACCOUNT_TAG = "ordering-account";
@@ -53,15 +53,6 @@ public abstract class AbstractReportGenerator implements ReportGenerator {
 
 	protected abstract void endWrite();
 
-	@FunctionalInterface
-	protected interface WriteOperation {
-
-		public static final WriteOperation noOp = () -> {
-		};
-
-		void execute() throws Exception;
-	}
-
 	protected void writeAndHandleException(WriteOperation writeOperation, WriteOperation onException, String message) {
 		try {
 			writeOperation.execute();
@@ -73,6 +64,15 @@ public abstract class AbstractReportGenerator implements ReportGenerator {
 			}
 			throw new ReportGeneratorException(message, e1);
 		}
+	}
+
+	@FunctionalInterface
+	protected interface WriteOperation {
+
+		public static final WriteOperation noOp = () -> {
+		};
+
+		void execute() throws Exception;
 	}
 
 	private void validateSettings() {
@@ -109,23 +109,29 @@ public abstract class AbstractReportGenerator implements ReportGenerator {
 	@SuppressWarnings("rawtypes")
 	private void writeOrderingAccountInfo(AccountDTO orderingAccount) {
 		LinkedList<ReportNode> subNodes = new LinkedList<>();
-		subNodes.add(new TerminalReportNode(ACCOUNT_NUMBER_TAG, orderingAccount.getAccountNumber()));
-		subNodes.add(new TerminalReportNode(ACCOUNT_IBAN_TAG, orderingAccount.getIban().getIBANValue()));
-		subNodes.add(new TerminalReportNode(ACCOUNT_NAME_TAG, orderingAccount.getAccountName()));
-		subNodes.add(new TerminalReportNode(CUSTOMER_NAME_TAG, orderingAccount.getCustomerDTO().getName()));
-		subNodes.add(new TerminalReportNode(BALANCE_TAG, orderingAccount.getBalance().toString()));
-		subNodes.add(new TerminalReportNode(CURRENCY_TAG, orderingAccount.getCurreny().getDisplayName()));
-		subNodes.add(new TerminalReportNode(STATUS_TAG, orderingAccount.getAccountStatus().toString()));
-		writeNode(new HierarchicalReportNode(ORDERING_ACCOUNT_TAG, subNodes));
+		subNodes.add(newNode(ACCOUNT_NUMBER_TAG, orderingAccount.getAccountNumber()));
+		subNodes.add(newNode(ACCOUNT_IBAN_TAG, orderingAccount.getIban().getIBANValue()));
+		subNodes.add(newNode(ACCOUNT_NAME_TAG, orderingAccount.getAccountName()));
+		subNodes.add(newNode(CUSTOMER_NAME_TAG, orderingAccount.getCustomerDTO().getName()));
+		subNodes.add(newNode(BALANCE_TAG, orderingAccount.getBalance().toString()));
+		subNodes.add(newNode(CURRENCY_TAG, orderingAccount.getCurreny().getDisplayName()));
+		subNodes.add(newNode(STATUS_TAG, orderingAccount.getAccountStatus().toString()));
+		writeNode(newNode(ORDERING_ACCOUNT_TAG, subNodes));
 	}
 
 	private void writeBeneficiaryInfo(PaymentDTO payment, String transcribedPayment) {
-		writeNode(new TerminalReportNode(BENEFICIARY_IBAN_TAG, payment.getBeneficiaryIBAN().getIBANValue()));
-		writeNode(new TerminalReportNode(BENEFICIARY_NAME_TAG, payment.getBeneficiaryName()));
-		writeNode(new TerminalReportNode(PAYMENT_AMOUNT_TAG, String.valueOf(payment.getPaymentAmount())));
-		writeNode(new TerminalReportNode(AMOUNT_TRANSCRIPTION_TAG, transcribedPayment));
-		writeNode(new TerminalReportNode(TRANSFER_CURRENCY_TAG, payment.getTransferCurrency().getDisplayName()));
-		writeNode(new TerminalReportNode(PAYMENT_DATE_TAG, payment.getPaymentDate().toString()));
-		writeNode(new TerminalReportNode(PAYMENT_PURPOSE_TAG, payment.getPaymentPurpose().getDescription()));
+		writeNode(newNode(BENEFICIARY_IBAN_TAG, payment.getBeneficiaryIBAN().getIBANValue()));
+		writeNode(newNode(BENEFICIARY_NAME_TAG, payment.getBeneficiaryName()));
+		writeNode(newNode(PAYMENT_AMOUNT_TAG, String.valueOf(payment.getPaymentAmount())));
+		writeNode(newNode(PAYMENT_AMOUNT_TRANSCRIPTION_TAG, transcribedPayment));
+		writeNode(newNode(TRANSFER_CURRENCY_TAG, payment.getTransferCurrency().getDisplayName()));
+		writeNode(newNode(PAYMENT_DATE_TAG, payment.getPaymentDate().toString()));
+		writeNode(newNode(PAYMENT_PURPOSE_TAG, payment.getPaymentPurpose().getDescription()));
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private ReportNode newNode(String name, Object value) {
+		return (value instanceof String) ? new TerminalReportNode(name, (String) value)
+				: new HierarchicalReportNode(name, (Iterable<ReportNode>) value);
 	}
 }
