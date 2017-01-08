5f3ecd810df5287d60378c228b8cad6f1daad2ce
ahmad jaradat <ahamd.jaradat@progressoft.com>
2017-01-08 17:07:48 +0200

    645

--- payment-parent/payment-web/src/main/java/com/progressoft/jip/ui/web/form/WebFormController.java
+++ payment-parent/payment-web/src/main/java/com/progressoft/jip/ui/web/form/WebFormController.java
@@ -1,8 +1,12 @@
 package com.progressoft.jip.ui.web.form;
 
+import java.io.InputStream;
+import java.util.Iterator;
+
 import com.progressoft.jip.PaymentMenuContext;
 import com.progressoft.jip.ui.web.form.manger.SingleWebFormManger;
 import com.progressoft.jip.ui.web.form.parameter.ParameterParser;
+import com.progressoft.jip.ui.web.form.parameter.ParameterValueTuple;
 
 public class WebFormController<T> {
 	
@@ -12,6 +16,15 @@ public class WebFormController<T> {
 	public WebFormController(ParameterParser parameterParser, SingleWebFormManger<PaymentMenuContext, T> single) {
 		this.parameterParser = parameterParser;
 		this.single = single;
+	}
+
+	public void process(InputStream is) {
+		Iterable<ParameterValueTuple> parameters = parameterParser.getParameters(is);
+		Iterator<ParameterValueTuple> iterator = parameters.iterator();
+		while(iterator.hasNext()){
+			ParameterValueTuple next = iterator.next();
+			single.submitFieldValue(next.getParameterName(), next.getValue());
+		}
 	}
 
 	
--- payment-parent/payment-web/src/test/java/com/progressoft/jip/ui/web/form/WebFormControllerTest.java
+++ payment-parent/payment-web/src/test/java/com/progressoft/jip/ui/web/form/WebFormControllerTest.java
@@ -1,16 +1,52 @@
 package com.progressoft.jip.ui.web.form;
 
-import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import com.progressoft.jip.PaymentMenuContext;
+import com.progressoft.jip.ui.web.form.manger.SingleWebFormManger;
+import com.progressoft.jip.ui.web.form.parameter.ParameterParser;
+import com.progressoft.jip.ui.web.form.parameter.StreamParameterParser;
 import com.progressoft.jip.ui.web.form.parameter.TestDto;
 
 public class WebFormControllerTest {
+	
+	private static final String PARAMETER_1 = "parameter_1";
+	private static final String VALUE_1 = "value_1";
+	private static final String VALUE_2 = "value_2";
+	private static final String PARAMETER_2 = "parameter_2";;
+	
 
 	private WebFormController<TestDto> webFormController;
-	
-	@Test
-	public void whenSetFieldThenCallSingleFormSetValueMethod() {
-		
+	private SingleWebFormManger<PaymentMenuContext, TestDto> singleWebFormManger;
+	private ParameterParser parameterParser;
+
+	@Before
+	public void init() {
+		parameterParser = new StreamParameterParser(); 
+		singleWebFormManger = mock(SingleWebFormManger.class);
+		webFormController = new WebFormController(parameterParser, singleWebFormManger);
 	}
 
+	@Test
+	public void whenCallProcessStreamWithOneParameterThenCallSingleFormSetValueMethod() {
+		InputStream is = new ByteArrayInputStream("parameter_1=value_1".getBytes());
+		webFormController.process(is);
+		verify(singleWebFormManger, times(1)).submitFieldValue("parameter_1", "value_1");
+	}
+	
+	@Test
+	public void whenCallProcessStreamWithTwoParameterThenCallSingleFormSetValueMethod() {
+		InputStream is = new ByteArrayInputStream(
+				(PARAMETER_1 + "=" + VALUE_1 + "&" + PARAMETER_2 + "=" + VALUE_2).getBytes());
+		webFormController.process(is);
+		verify(singleWebFormManger, times(1)).submitFieldValue(PARAMETER_1, VALUE_1);
+		verify(singleWebFormManger, times(1)).submitFieldValue(PARAMETER_2, VALUE_2);
+	}
+
+	
 }
