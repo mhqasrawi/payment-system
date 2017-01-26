3d4fc51d2e987fa04c0e8609e6425fce1d01bdb5
Talal Alrawajfeh <TalalAlrawajfeh@gmail.com>
2017-01-26 09:43:23 +0200

    Code formatting and optimizing imports for payment-main

--- payment-parent/payment-main/src/main/java/com/progressoft/jip/Main.java
+++ payment-parent/payment-main/src/main/java/com/progressoft/jip/Main.java
@@ -1,30 +1,29 @@
 package com.progressoft.jip;
 
-import java.io.FileNotFoundException;
-
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-
 import com.progressoft.jip.ui.menu.MenuContext;
 import com.progressoft.jip.ui.menu.MenuRenderManger;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import java.io.FileNotFoundException;
 
 public class Main {
 
-	private static final String APP_CONTEXT_LOCATION = "app.context.location";
-	public static final PaymentMenuContext context = new MenuContextImpl();
-	public static final MenuRenderManger<PaymentMenuContext> renderManger = new ConsoleMenuRender<>(
-			System.in, System.out, context);
+    public static final PaymentMenuContext context = new MenuContextImpl();
+    public static final MenuRenderManger<PaymentMenuContext> renderManger = new ConsoleMenuRender<>(
+            System.in, System.out, context);
+    private static final String APP_CONTEXT_LOCATION = "app.context.location";
 
-	private Main() {
+    private Main() {
 
-	}
+    }
 
-	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-		String appContextLocation = System.getProperty(APP_CONTEXT_LOCATION);
-		FileSystemXmlApplicationContext appContext = new FileSystemXmlApplicationContext(appContextLocation);
-		XmlFileUserInterfaceGenerator uiGenerator = appContext.getBean(XmlFileUserInterfaceGenerator.class);
-		context.put(MenuContext.MENU_RENDER_MANGER, renderManger);
-		renderManger.renderMenu(uiGenerator.generateUi());
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        String appContextLocation = System.getProperty(APP_CONTEXT_LOCATION);
+        FileSystemXmlApplicationContext appContext = new FileSystemXmlApplicationContext(appContextLocation);
+        XmlFileUserInterfaceGenerator uiGenerator = appContext.getBean(XmlFileUserInterfaceGenerator.class);
+        context.put(MenuContext.MENU_RENDER_MANGER, renderManger);
+        renderManger.renderMenu(uiGenerator.generateUi());
 
-	}
+    }
 
 }
