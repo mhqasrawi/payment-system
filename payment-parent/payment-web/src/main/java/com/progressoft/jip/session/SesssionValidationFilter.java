fe1ebf8232a21d2b2744d29883cf1799ff169abe
PROGRESSOFT\u612 <ahmad.jaradat@progressoft.com>
2017-01-31 16:53:41 +0200

    Connect Pages With Each Other

---
+++ payment-parent/payment-web/src/main/java/com/progressoft/jip/session/SesssionValidationFilter.java
@@ -0,0 +1,43 @@
+package com.progressoft.jip.session;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * Created by u612 on 1/31/2017.
+ */
+@WebFilter(urlPatterns = "/*")
+public class SesssionValidationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String requestURI = req.getRequestURI();
+        if ("/pikup_account".equalsIgnoreCase(requestURI)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        HttpSession session = req.getSession(false);
+        if (Objects.isNull(session)) {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.sendRedirect(req.getContextPath() + "/pikup_account");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
